b05ebdd52e46870617706ff8a84d8e9b73f4863c
Andreas Tennert <andreas@qupaya.com>
2023-08-08 13:43:40 +0200

    feat: add server info awareness for newsletter registration

--- src/main/kotlin/com/qupaya/brevo/NewsletterRegistrationEventListenerProviderFactory.kt
+++ src/main/kotlin/com/qupaya/brevo/NewsletterRegistrationEventListenerProviderFactory.kt
@@ -1,15 +1,24 @@
 package com.qupaya.brevo
 
+import org.apache.http.client.methods.HttpPost
+import org.apache.http.entity.StringEntity
+import org.apache.http.impl.client.DefaultHttpRequestRetryHandler
+import org.apache.http.impl.client.HttpClientBuilder
+import org.apache.http.impl.client.LaxRedirectStrategy
+import org.apache.http.util.EntityUtils
 import org.jboss.logging.Logger
 import org.keycloak.Config
 import org.keycloak.events.EventListenerProvider
 import org.keycloak.events.EventListenerProviderFactory
 import org.keycloak.models.KeycloakSession
 import org.keycloak.models.KeycloakSessionFactory
+import org.keycloak.provider.ServerInfoAwareProviderFactory
+import java.io.IOException
 import java.util.concurrent.Executors
 
 
-class NewsletterRegistrationEventListenerProviderFactory : EventListenerProviderFactory {
+class NewsletterRegistrationEventListenerProviderFactory : EventListenerProviderFactory,
+    ServerInfoAwareProviderFactory {
     private val threadPool = Executors.newFixedThreadPool(1)
 
     override fun create(session: KeycloakSession): EventListenerProvider {
@@ -29,6 +38,49 @@ class NewsletterRegistrationEventListenerProviderFactory : EventListenerProvider
     }
 
     override fun getId(): String = ID
+
+    override fun getOperationalInfo(): Map<String, String> {
+        val brevoFormLink = System.getenv("BREVO_FORM_LINK")
+        var brevoRequestResult = "---"
+
+        if (!brevoFormLink.isNullOrEmpty()) {
+            val httpPost = HttpPost(brevoFormLink)
+            httpPost.entity = StringEntity("")
+            httpPost.setHeader("content-type", "application/x-www-form-urlencoded")
+
+            try {
+                HttpClientBuilder.create()
+                    .setRetryHandler(DefaultHttpRequestRetryHandler())
+                    .setRedirectStrategy(LaxRedirectStrategy()).build()
+                    .use { http ->
+                        val response = http.execute(httpPost)
+                        if (response.statusLine.statusCode >= 400) {
+                            LOG.error("Brevo newsletter subscription test request response: ${response.statusLine.statusCode}")
+                            LOG.error(EntityUtils.toString(response.entity))
+                            brevoRequestResult = "Brevo newsletter subscription test request response: ${response.statusLine.statusCode}"
+                        }
+                    }
+
+            } catch (ex: IOException) {
+                LOG.error(
+                    "IO exception while sending newsletter subscription test request",
+                    ex
+                )
+                brevoRequestResult = "IO exception while sending newsletter subscription test request"
+            } catch (ex: InterruptedException) {
+                LOG.error(
+                    "Interruption while sending newsletter subscription test request.",
+                    ex
+                )
+                brevoRequestResult = "Interruption while sending newsletter subscription test request."
+            }
+        }
+
+        return mapOf(
+            "brevoFormLink" to brevoFormLink,
+            "testRequestResult" to brevoRequestResult,
+        )
+    }
 
     companion object {
         private const val ID = "brevo-newsletter-registration-event-listener"
