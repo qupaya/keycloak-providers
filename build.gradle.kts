f48dae6e459251b7cabeaeb9f9487f77327a891a
Gerrit Letz <gerrit@qupaya.com>
2024-08-13 10:13:32 +0200

    chore: increase keycloak version to 24.0.5

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,17 +5,17 @@ plugins {
 }
 
 group = "com.qupaya"
-version = "v0.0.11"
+version = "v0.0.12"
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    implementation("org.keycloak:keycloak-core:24.0.4")
-    implementation("org.keycloak:keycloak-services:24.0.4")
-    implementation("org.keycloak:keycloak-server-spi:24.0.4")
-    implementation("org.keycloak:keycloak-server-spi-private:24.0.4")
+    implementation("org.keycloak:keycloak-core:24.0.5")
+    implementation("org.keycloak:keycloak-services:24.0.5")
+    implementation("org.keycloak:keycloak-server-spi:24.0.5")
+    implementation("org.keycloak:keycloak-server-spi-private:24.0.5")
     implementation("com.google.guava:guava:31.1-jre")
     implementation("org.mindrot:jbcrypt:0.4")
     implementation("javax.ws.rs:javax.ws.rs-api:2.1.1")
